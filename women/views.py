5e5e160759b17612bb36b93ec5f7b944744dd382
Aichurok <toychubekova2328.com>
2024-06-26 22:11:14 +0600

    18 lesson

--- women/views.py
+++ women/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
@@ -9,6 +10,7 @@ from .models import *
 from .utils import *
 
 class WomenHome(DataMixin, ListView):
+
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -35,7 +37,12 @@ class WomenHome(DataMixin, ListView):
 #     return render(request, 'women/index.html', context=context)
 
 def about(request):
-    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+    contact_list = Women.objects.all()
+    paginator = Paginator(contact_list, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'women/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
 
 
 class Addpage(LoginRequiredMixin, DataMixin, CreateView):
