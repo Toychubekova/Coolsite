148c023fc3aa0ad17b5bdc46da4ee3abeb96c937
Aichurok <toychubekova2328.com>
2024-06-27 14:18:47 +0600

    22 lesson

--- women/utils.py
+++ women/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-
+from django.core.cache import cache
 from women.models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -12,7 +12,11 @@ class DataMixin:
     paginate_by = 2
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(num_posts=Count('women'))
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('women'))
+            cache.set('cats', cats,60)
+
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
             user_menu.pop(1)
